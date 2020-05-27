a669406a4f1ef59c5a1cfe58cf17dd860cfaddc4
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-27 16:54:37 +0530

    Handled fetch all products using axios

--- e-commerce-shopping-frontend/src/components/Dashboard.js
+++ e-commerce-shopping-frontend/src/components/Dashboard.js
@@ -1,34 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductList from './Product/ProductList';
+import axios from 'axios';
 
 export default function Dashboard() {
-    const products = [{
-        _id: '1',
-        name: 'Polo T-shirt',
-        brand: 'Nike',
-        price: 1200,
-        image: 'https://allensolly.imgix.net/img/app/product/2/290068-1240382.jpg'
-    }, {
-        _id: '2',
-        name: 'Regular Fit Polo T-Shirt',
-        brand: 'Allen Solly',
-        price: 2200,
-        image: 'https://allensolly.imgix.net/img/app/product/3/321962-1510640.jpg?auto=format'
-    }, {
-        _id: '3',
-        name: "Casual Formal Shirt",
-        brand: 'Nike',
-        price: 1200,
-        image: "https://images-na.ssl-images-amazon.com/images/I/61DLuz2Of5L._UY445_.jpg"
-    }, {
-        _id: '4',
-        name: 'Polo T-shirt (Black)',
-        brand: 'Nike',
-        price: 1200,
-        image: "https://www.ubexpress.pk/wp-content/uploads/2019/06/Black-Nike-AA-11.jpg"
-    }];
-        return (
-            <ProductList products = {products}/>
-        )
+    
+    const [products,setProducts] = useState([]);
+    useEffect(()=> {
+        axios.get("http://localhost:5000/api/products")
+        .then((res)=> setProducts(res.data.products))
+        .catch((err)=> console.log(err.response.data));
+    },[])
+    return (
+        <ProductList products = {products}/>
+    )
 }
 
