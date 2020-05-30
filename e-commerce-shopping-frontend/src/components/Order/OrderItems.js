d7618df0c7a9dffd566a9fe32f7bc8b64b657e12
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-30 16:21:31 +0530

    Modified Order Item component with real order list

--- e-commerce-shopping-frontend/src/components/Order/OrderItems.js
+++ e-commerce-shopping-frontend/src/components/Order/OrderItems.js
@@ -1,35 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class OrderItems extends Component {
-    render() {
+export default function OrderItems(props) {
         return (
             <React.Fragment>
                 <div className="row">
                     <div className="col-lg-2 col-md-2">
-                        <img className="img-rounded rounded-circle" style={{"width": "5.5rem", "height": "5.5rem"}} src="https://images-na.ssl-images-amazon.com/images/I/61DLuz2Of5L._UY445_.jpg" alt="Cart item"/>
+                        <img className="img-rounded rounded-circle" style={{"width": "5.5rem", "height": "5.5rem"}} src={props.item.image} alt="Ordered item"/>
                     </div>
                     <div className="col-lg-8 col-md-8 pl-4">
-                        <h6>Casual Formal Shirt</h6>
-                        <p>Quantity: 3</p>
+                        <h6>{props.item.name}</h6>
+                        <p>Quantity: {props.item.quantityOrdered}</p>
                     </div>
                     <div className="col-lg-2 col-md-2 pl-4">
-                        <p>2500/-</p>
+                        <p>{props.item.price}/-</p>
                     </div>
                 </div>
                 <hr/>
-                <div className="row">
-                    <div className="col-lg-2 col-md-2">
-                        <img className="img-rounded rounded-circle" style={{"width": "5.5rem", "height": "5.5rem"}} src="https://images-na.ssl-images-amazon.com/images/I/61DLuz2Of5L._UY445_.jpg" alt="Cart item"/>
-                    </div>
-                    <div className="col-lg-8 col-md-8 pl-4">
-                        <h6>Casual Formal Shirt</h6>
-                        <p>Quantity: 3</p>
-                    </div>
-                    <div className="col-lg-2 col-md-2 pl-4">
-                        <p>2500/-</p>
-                    </div>
-                </div>
             </React.Fragment>
         )
-    }
 }
--- e-commerce-shopping-frontend/src/components/Order/PlaceOrder.js
+++ e-commerce-shopping-frontend/src/components/Order/PlaceOrder.js
@@ -1,66 +1,87 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import OrderItems from './OrderItems'
 import { connect } from 'react-redux';
 
-class PlaceOrder extends Component {
-    render() {
-        const {shippingDetails, paymentMethod} = this.props;
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8" style={{marginTop: "2%"}}>
-                        <div className="row">
-                            <div className="card bg-light text-dark" style={{width: "90%", margin: "auto"}}>
-                                <div className="card-body">
-                                    <h5 className="card-title"><b>Shipping Address</b></h5>
-                                    <p className="card-text">{shippingDetails.address}, {shippingDetails.city}, {shippingDetails.country}, {shippingDetails.postalCode}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <br/>
-                        <div className="row">
-                            <div className="card bg-light text-dark" style={{width: "90%", margin: "auto"}}>
-                                <div className="card-body">
-                                    <h5 className="card-title"><b>Payment Method</b></h5>
-                                    <p className="card-text">{paymentMethod.method}</p>
-                                </div>
-                            </div>
-                        </div>   
-                        <br/>
-                        <div className="card bg-light text-dark" style={{width: "94%", margin: "auto"}}>   
+function PlaceOrder(props){
+    
+    const [subTotal, setSubTotal] = useState(0);
+    const {shippingDetails, paymentMethod, cart} = props;
+
+    useEffect(() => {
+        let cartProduct;
+        if (props.cart.length === 0) {
+            cartProduct = [{
+                "price": 0
+            }];
+        } else {
+            cartProduct = props.cart
+        }
+        const totalCost = cartProduct.reduce((acc, cur) => ({
+            price: acc.price + cur.price
+        }));
+        setSubTotal(totalCost.price);
+    }, [props.cart])
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8" style={{marginTop: "2%"}}>
+                    <div className="row">
+                        <div className="card bg-light text-dark" style={{width: "90%", margin: "auto"}}>
                             <div className="card-body">
-                                    <h5 className="card-title"><b>Order Items</b></h5>
-                                    <OrderItems/>
-                            </div>     
-                            
+                                <h5 className="card-title"><b>Shipping Address</b></h5>
+                                <p className="card-text">{shippingDetails.address}, {shippingDetails.city}, {shippingDetails.country}, {shippingDetails.postalCode}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-4" style={{margin: "2% 0", padding: "0%"}}>
-                        <div className="card" style={{"width": "18rem", margin: "auto" }}>
-                            <button className="btn btn-warning">Place Order</button>
+                    <br/>
+                    <div className="row">
+                        <div className="card bg-light text-dark" style={{width: "90%", margin: "auto"}}>
                             <div className="card-body">
-                                <h5 className="card-title"><b>Order Summary</b></h5>
-                                <p className="card-text">Items: 3600/- INR</p>
-                                <p className="card-text">Shipping: Free</p>                                
-                                <p className="card-text">Tax: 130/- INR</p>
-                                <h5 className="card-title">Order Total: 3730/- INR</h5>
+                                <h5 className="card-title"><b>Payment Method</b></h5>
+                                <p className="card-text">{paymentMethod.method}</p>
                             </div>
+                        </div>
+                    </div>   
+                    <br/>
+                    <div className="card bg-light text-dark" style={{width: "94%", margin: "auto"}}>   
+                        <div className="card-body">
+                            <h5 className="card-title"><b>Order Items</b></h5>
+                            {cart.map((item)=> {
+                                return (
+                                    <OrderItems key={item._id} item={item}/>
+                                )
+                            })}
+                        </div>      
+                    </div>
+                </div>
+                <div className="col-lg-4" style={{margin: "2% 0", padding: "0%"}}>
+                    <div className="card" style={{"width": "18rem", margin: "auto" }}>
+                        <button className="btn btn-warning">Place Order</button>
+                        <div className="card-body">
+                            <h5 className="card-title"><b>Order Summary</b></h5>
+                            <p className="card-text">Items: {subTotal}/- INR</p>
+                            <p className="card-text">Shipping: Free</p>                                
+                            <p className="card-text">Tax: 130/- INR</p>
+                            <h5 className="card-title">Order Total: {subTotal + 130}/- INR</h5>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 PlaceOrder.defaultProps = {
     shippingDetails: {},
-    paymentMethod: {}
+    paymentMethod: {},
+    cart: []
 }
 const mapStateToProps = state => {
     return {
         shippingDetails: state.shipping.shippingAddress,
-        paymentMethod: state.shipping.paymentMethod
+        paymentMethod: state.shipping.paymentMethod,
+        cart: state.cart.cartProduct
     }
 }
 export default connect(mapStateToProps,null)(PlaceOrder);
