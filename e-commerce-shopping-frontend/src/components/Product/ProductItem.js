f91e8a10c63bd3797b53af56aa745bdf6244f227
meetparikh12 <parikhmeet1234@gmail.com>
2020-06-02 12:26:20 +0530

    Modified styling for products

--- e-commerce-shopping-frontend/src/components/Product/ProductItem.js
+++ e-commerce-shopping-frontend/src/components/Product/ProductItem.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import config from 'react-global-configuration';
-
 export default class ProductItem extends Component {
     render() {
         return (
-            
-            <div className="col-md-6 col-lg-4 mt-5" style={{padding: "0 2.2rem"}}>
-                <div className="card" style={{"width": "18rem"}}>
+            <div className="col-md-6 col-lg-4 mt-5 product_item" style={{padding: "0 2.2rem"}}>
+                <div className="card" style={{"width": "100%"}}>
                     <Link to={`/product/${this.props.id}`}>
                     <img className="card-img-top" src={`${config.get('backend_asset_url')}/${this.props.image}`} style={{height: "17.5rem", borderBottom: "1px solid rgba(0,0,0,.1)"}} alt="Product-Post"/>
                     </Link>
